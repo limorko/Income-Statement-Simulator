6f4f05f9693456b4c91501120d7308003587ad02
Limor Kohanim <78884296+limorko@users.noreply.github.com>
2021-04-15 14:29:56 +0200

    Update Income Statement - Limor K.py

--- Income Statement - Limor K.py
+++ Income Statement - Limor K.py
@@ -1,7 +1,4 @@
 #"I hereby certify that this program is solely the result of my own work 
-#and is in compliance with the Academic Integrity policy of the course syllabus
-#and the academic integrity policy of the CS department."
-
 import Draw
 
 #canvas' width, canvas' height
